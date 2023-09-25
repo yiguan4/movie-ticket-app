02683fbe1c2a6ced3d663f3a7f94f212a56d9f2f
yiguan4 <0420viviane@gmail.com>
2023-09-24 21:13:17 -0700

    Connect to localhost1234

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Networking;
 
 public class Login : MonoBehaviour
 {
+    [SerializeField] private string authenticationEndpoint = "http://127.0.0.1:1234/account";
+
     [SerializeField] private TMP_InputField usernameInputField;
     [SerializeField] private TMP_InputField passwordInputField;
 
     public void OnLoginClick() {
+        StartCoroutine(TryLogin());
+
+    }
+
+    private IEnumerator TryLogin() {
         string username = usernameInputField.text;
         string password = passwordInputField.text;
 
-        Debug.Log($"{username}:{password}");
+        UnityWebRequest request = UnityWebRequest.Get($"{authenticationEndpoint}?cUserName={username}&cPassword={password}");
+        var handler = request.SendWebRequest();
 
+        float startTime = 0.0f;
+        while (!handler.isDone) {
+            startTime += Time.deltaTime;
+
+            if (startTime > 10.0f) {
+                break;
+            }
+
+            yield return null;
+        }
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log(request.downloadHandler.text);
+        }
+        else
+        {
+            Debug.Log("Unable to connect to the server");
+        }
+
+
+        yield return null;
     }
 }
