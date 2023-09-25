fd5041f0024b58c1ffe2e5bcca7fab212a8413d2
yiguan4 <0420viviane@gmail.com>
2023-09-25 11:44:05 -0700

    Split login & register logic

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -7,37 +7,50 @@ using UnityEngine.UI;
 
 public class Login : MonoBehaviour
 {
-    [SerializeField] private string authenticationEndpoint = "http://127.0.0.1:1234/account";
+    [SerializeField] private string loginEndpoint = "http://127.0.0.1:1234/account/login";
+    [SerializeField] private string createEndpoint = "http://127.0.0.1:1234/account/create";
 
 
-    [SerializeField] private TextMeshProUGUI alertText;
-    [SerializeField] private Button loginButton;
+    [SerializeField] private TextMeshProUGUI alertLoginText;
+    [SerializeField] private TextMeshProUGUI alertRegisterText;
+    // [SerializeField] private Button loginButton;
+    // [SerializeField] private Button createButton;
     [SerializeField] private TMP_InputField usernameInputField;
     [SerializeField] private TMP_InputField passwordInputField;
 
-    public void OnLoginClick() {
-        alertText.text = "Signing in...";
-        loginButton.interactable = false;
+    public void OnLoginClick()
+    {
+        alertLoginText.text = "Signing in...";
+        //  loginButton.interactable = false;
         StartCoroutine(TryLogin());
 
     }
 
-    private IEnumerator TryLogin() {
+    public void OnCreateClick()
+    {
+        alertRegisterText.text = "Registering account...";
+        //   createButton.interactable = false;
+        StartCoroutine(TryCreate());
+    }
+
+    private IEnumerator TryLogin()
+    {
 
 
         string username = usernameInputField.text;
         string password = passwordInputField.text;
 
-        if (username.Length < 3 || username.Length > 24) {
-            alertText.text = "Invalid username";
-            loginButton.interactable = true;
+        if (username.Length < 3 || username.Length > 24)
+        {
+            alertLoginText.text = "Invalid username";
+            //      loginButton.interactable = true;
             yield break;
         }
 
         if (password.Length < 3 || password.Length > 24)
         {
-            alertText.text = "Invalid password";
-            loginButton.interactable = true;
+            alertLoginText.text = "Invalid password";
+            //     loginButton.interactable = true;
             yield break;
         }
 
@@ -45,14 +58,16 @@ public class Login : MonoBehaviour
         form.AddField("cUsername", username);
         form.AddField("cPassword", password);
 
-        UnityWebRequest request = UnityWebRequest.Post(authenticationEndpoint, form);
+        UnityWebRequest request = UnityWebRequest.Post(loginEndpoint, form);
         var handler = request.SendWebRequest();
 
         float startTime = 0.0f;
-        while (!handler.isDone) {
+        while (!handler.isDone)
+        {
             startTime += Time.deltaTime;
 
-            if (startTime > 10.0f) {
+            if (startTime > 10.0f)
+            {
                 break;
             }
 
@@ -61,25 +76,93 @@ public class Login : MonoBehaviour
 
         if (request.result == UnityWebRequest.Result.Success)
         {
-            if (request.downloadHandler.text != "Invalid credentials") {
-                loginButton.interactable = false;
+            if (request.downloadHandler.text != "Invalid credentials")
+            {
+                //        loginButton.interactable = false;
                 UserAccount returnedAccount = JsonUtility.FromJson<UserAccount>(request.downloadHandler.text);
-                alertText.text = $"{returnedAccount._id} Welcome" + returnedAccount.username;
+                alertLoginText.text = $"{returnedAccount._id} Welcome" + returnedAccount.username;
             }
-            else {
-                alertText.text = "Invalid credentials";
-                loginButton.interactable = true;
+            else
+            {
+                alertLoginText.text = "Invalid credentials";
+                //    loginButton.interactable = true;
             }
 
 
         }
         else
         {
-            alertText.text = "Error connecting to the server...";
-            loginButton.interactable = true;
+            alertLoginText.text = "Error connecting to the server...";
+            //   loginButton.interactable = true;
         }
 
 
         yield return null;
     }
+
+    private IEnumerator TryCreate()
+    {
+        string username = usernameInputField.text;
+        string password = passwordInputField.text;
+
+        if (username.Length < 3 || username.Length > 24)
+        {
+            alertRegisterText.text = "Invalid username";
+            //      loginButton.interactable = true;
+            yield break;
+        }
+
+        if (password.Length < 3 || password.Length > 24)
+        {
+            alertRegisterText.text = "Invalid password";
+            //     loginButton.interactable = true;
+            yield break;
+        }
+
+        WWWForm form = new WWWForm();
+        form.AddField("cUsername", username);
+        form.AddField("cPassword", password);
+
+        UnityWebRequest request = UnityWebRequest.Post(createEndpoint, form);
+        var handler = request.SendWebRequest();
+
+        float startTime = 0.0f;
+        while (!handler.isDone)
+        {
+            startTime += Time.deltaTime;
+
+            if (startTime > 10.0f)
+            {
+                break;
+            }
+
+            yield return null;
+        }
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            if (request.downloadHandler.text != "Invalid credentials" && request.downloadHandler.text != "Username is already taken")
+            {
+                //        loginButton.interactable = false;
+                UserAccount returnedAccount = JsonUtility.FromJson<UserAccount>(request.downloadHandler.text);
+                alertRegisterText.text = "Account has been created";
+            }
+            else
+            {
+                alertRegisterText.text = "Invalid credentials";
+                //    loginButton.interactable = true;
+            }
+
+
+        }
+        else
+        {
+            alertRegisterText.text = "Error connecting to the server...";
+            //   loginButton.interactable = true;
+        }
+
+
+        yield return null;
+    }
+
 }
