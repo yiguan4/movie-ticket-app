9ca84b678a4903493d1625c0b72003c4c789f44a
yiguan4 <0420viviane@gmail.com>
2023-09-26 21:25:58 -0700

    Clean up UI

--- Assets/Scripts/ProfilePage.cs
+++ Assets/Scripts/ProfilePage.cs
@@ -6,10 +6,16 @@ using TMPro;
 public class ProfilePage : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI alertFirstnameText;
+    [SerializeField] private TextMeshProUGUI alertLastnameText;
+    [SerializeField] private TextMeshProUGUI alertEmailText;
+    [SerializeField] private TextMeshProUGUI alertUsernameText;
 
-    public void Awake()
+    public void Update()
     {
         alertFirstnameText.text = "First Name: " + Login.returnedAccount.firstname;
+        alertLastnameText.text = "Last Name: " + Login.returnedAccount.lastname;
+        alertEmailText.text = "Email: " + Login.returnedAccount.email;
+        alertUsernameText.text = "Username: " + Login.returnedAccount.username;
     }
 
 
